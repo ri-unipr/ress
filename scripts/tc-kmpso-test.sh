3480c108f6105becc25744eb07e26d7f444293b6
ardarico <michele.amoretti@unipr.it>
2020-03-31 15:25:54 +0200

    fix to the tc test script

---
+++ scripts/tc-kmpso-test.sh
@@ -0,0 +1,1 @@
+../bin/kmpso 26 2000 1 3 501 20 100 50 ../scripts/systems/cstr_26.txt output-tc.txt tc
--- scripts/tci-kmpso-test.sh
+++
@@ -1,2 +0,0 @@
-../bin/kmpso 26 2000 1 3 501 20 100 50 ../scripts/systems/cstr_26.txt output-tci.txt tci
-
