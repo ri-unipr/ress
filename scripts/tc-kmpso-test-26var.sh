8a8cdc4528a7ecce8a9fcab8f0bdae08aa95472f
ardarico <michele.amoretti@unipr.it>
2020-05-14 20:38:13 +0200

    fixed variable names in kmpso scripts for 26-var system

--- scripts/tc-kmpso-test-26var.sh
+++ scripts/tc-kmpso-test-26var.sh
@@ -1,1 +1,1 @@
-../bin/kmpso 26 2000 1 3 501 20 100 50 123456 ../scripts/systems/cstr_26.txt output-tc.txt tc "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z" 0
+../bin/kmpso 26 2000 1 3 501 20 100 50 123456 ../scripts/systems/cstr_26.txt output-tc.txt tc "A AA AAA AAAA AAAB AAA_AABBA AAB AABBA AAB_AAAB AA_AAAA AB ABA ABB ABBBBA ABB_BAB B BA BAA BAAB BAA_BBBABA BAB BB BBA BBB BBBABA BBB_ABBBBA" 0 hs-kmpso.txt 
