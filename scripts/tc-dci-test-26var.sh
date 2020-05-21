fabcf66c9c2c29f14f5110abe7592f8b8f16cf88
ardarico <michele.amoretti@unipr.it>
2020-05-21 16:21:30 +0200

    updated scripts with better output file names

--- scripts/tc-dci-test-26var.sh
+++ scripts/tc-dci-test-26var.sh
@@ -1,1 +1,1 @@
-../bin/dci ../scripts/systems/cstr_26.txt --rand-seed:123456 --tc --out:output-tc-26var.txt --hs-out:hs-dci-26var.txt --verbose
+../bin/dci ../scripts/systems/cstr_26.txt --rand-seed:123456 --tc --out:output-tc-dci-26var.txt --hs-out:hs-dci-26var.txt --verbose
